820386067122177c83c0e84b1fbbfaab6c810414
Samuel de Barros <samuel.barros@db1.com.br>
2026-02-24 10:40:44 -0300

    Base da classe GlobalExceptionHandler implementada

--- GestãoDeTurmas/Middlewares/GlobalExceptionHandler.cs
+++ GestãoDeTurmas/Middlewares/GlobalExceptionHandler.cs
@@ -4,8 +4,19 @@ namespace GestãoDeTurmas.Middlewares;
 
 public class GlobalExceptionHandler : IExceptionHandler
 {
-    public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+
+    private readonly ILogger<GlobalExceptionHandler> _logger;
+
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
     {
-        throw new NotImplementedException();
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "Um erro inesperado aconteceu na aplicação: {Mensagem}", exception.Message);
+
+        httpContext.Response.Redirect("Home/Error");
+        return true;
     }
 }
