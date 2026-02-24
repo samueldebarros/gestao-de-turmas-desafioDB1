c36fe72728a4cec6c14c12797086a7e61b589c15
Samuel de Barros <samuel.barros@db1.com.br>
2026-02-24 10:35:16 -0300

    criacao da pasta e classe GlobalExceptionHandler

---
+++ GestãoDeTurmas/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace GestãoDeTurmas.Middlewares;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
