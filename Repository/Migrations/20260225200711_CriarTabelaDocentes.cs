e3b9f70665733c02c2bf9fa82dc778e18eae07dd
Samuel de Barros <samuel.barros@db1.com.br>
2026-02-25 17:11:21 -0300

    Criacao da Tabela Docentes

---
+++ Repository/Migrations/20260225200711_CriarTabelaDocentes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarTabelaDocentes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Docentes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "VARCHAR(100)", maxLength: 100, nullable: false),
+                    Cpf = table.Column<string>(type: "VARCHAR(14)", maxLength: 14, nullable: false),
+                    Email = table.Column<string>(type: "VARCHAR(150)", maxLength: 150, nullable: true),
+                    DataNascimento = table.Column<DateOnly>(type: "DATE", nullable: false),
+                    Especialidade = table.Column<string>(type: "VARCHAR(50)", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Docentes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Docentes_Cpf",
+                table: "Docentes",
+                column: "Cpf",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Docentes");
+        }
+    }
+}
