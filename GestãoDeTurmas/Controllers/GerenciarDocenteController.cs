c615bf735cf1c558cce33acbfe3228a3846880ee
Samuel de Barros <samuel.barros@db1.com.br>
2026-02-26 10:46:04 -0300

    View/Docente/Index adicionado e GerenciarDocenteViewModel criado

---
+++ GestãoDeTurmas/Controllers/GerenciarDocenteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestãoDeTurmas.Controllers
+{
+    public class GerenciarDocenteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GestãoDeTurmas/Models/Docente/GerenciarDocenteViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace GestãoDeTurmas.Models.Docente;
+
+public class GerenciarDocenteViewModel
+{
+    public List<DocenteListaViewModel> DocentesCadastrados { get; set; } = new List<DocenteListaViewModel>();
+    public DocenteInputViewModel NovoDocente { get; set; } = new DocenteInputViewModel();
+}
