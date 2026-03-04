f38aa4b203b01be65c9985d0add25f00ec6a5e9f
Samuel de Barros <samuel.barros@db1.com.br>
2026-03-04 16:50:57 -0300

    feat: Validador de CPF implementado

---
+++ Common/Utils/ValidacaoCpf.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Common.Utils;
+
+public static class ValidacaoCpf
+{
+    public static bool IsCpfValido(string cpf)
+    {
+        // Remove caracteres não numéricos
+        cpf = Regex.Replace(cpf, @"\D", "");
+
+        // Verifica se tem 11 dígitos
+        if (cpf.Length != 11) return false;
+
+        // Rejeita CPFs com todos os dígitos iguais
+        if (Regex.IsMatch(cpf, @"^(\d)\1{10}$")) return false;
+
+        // Calcula o primeiro dígito verificador
+        int sum = 0;
+        for (int i = 0; i < 9; i++)
+        {
+            sum += (cpf[i] - '0') * (10 - i);
+        }
+        int remainder = sum % 11;
+        int digit1 = remainder < 2 ? 0 : 11 - remainder;
+
+        // Calcula o segundo dígito verificador
+        sum = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            sum += (cpf[i] - '0') * (11 - i);
+        }
+        remainder = sum % 11;
+        int digit2 = remainder < 2 ? 0 : 11 - remainder;
+
+        // Verifica se os dígitos calculados conferem
+        return (cpf[9] - '0') == digit1 && (cpf[10] - '0') == digit2;
+    }
+}
+
