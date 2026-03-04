0109876ed62399b5eb5c50b43c6e52741b76ca25
Samuel de Barros <samuel.barros@db1.com.br>
2026-03-04 10:13:04 -0300

    Criacao de DisciplinaRepository

---
+++ Repository/DisciplinaRepository.cs
@@ -0,0 +1,44 @@
+﻿using Common.Domains;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository;
+
+public class DisciplinaRepository : IDisciplinaRepository
+{
+    public Task AdicionarDisciplinaAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task EditarDisciplinaAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InativarDisciplinaAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Disciplina> ObterDisciplinaPorIdAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Disciplina> ObterInativoPorIdAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<(List<Disciplina>, int total)> ObterTodasAsDisciplinasAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task ReativarDisciplinaAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Repository/IDisciplinaRepository.cs
@@ -0,0 +1,19 @@
+﻿using Common.Domains;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository;
+
+public interface IDisciplinaRepository
+{
+    Task<Disciplina> ObterDisciplinaPorIdAsync();
+    Task<Disciplina> ObterInativoPorIdAsync();
+    Task<(List<Disciplina>, int total)> ObterTodasAsDisciplinasAsync();
+    Task AdicionarDisciplinaAsync();
+    Task EditarDisciplinaAsync();
+    Task InativarDisciplinaAsync();
+    Task ReativarDisciplinaAsync();
+    
+    
+}
