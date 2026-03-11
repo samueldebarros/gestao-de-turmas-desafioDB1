a0baec14815dd39eb81073acf9e1582d593fe37f
Samuel de Barros <samuel.barros@db1.com.br>
2026-03-11 09:54:59 -0300

    DocenteDTOs adaptadas

--- API/DTOs/DocenteDTOs/DocenteInputDTO.cs
+++ API/DTOs/DocenteDTOs/DocenteInputDTO.cs
@@ -5,6 +5,6 @@ public class DocenteInputDTO
     public string Nome { get; set; }
     public string Cpf { get; set; }
     public string? Email { get; set; }
-    public string Especialidade { get; set; }
+    public int? DisciplinaId { get; set; }
     public DateOnly? DataNascimento {  get; set; }
 }
--- API/DTOs/DocenteDTOs/EditarDocenteDTO.cs
+++ API/DTOs/DocenteDTOs/EditarDocenteDTO.cs
@@ -7,6 +7,6 @@ public class EditarDocenteDTO
     public int Id { get; set; }
     public string Nome { get; set; }
     public string? Email { get; set; }
-    public string Especialidade { get; set; }
+    public int? DisciplinaId { get; set; }
     public DateOnly DataNascimento { get; set; }
 }
