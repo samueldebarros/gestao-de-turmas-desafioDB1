e65a6cebc1859739154f64090c07fe7c494b873a
Samuel de Barros <samuel.barros@db1.com.br>
2026-03-16 10:37:34 -0300

    feat: Paginacao componentizada

---
+++ GestãoDeTurmas/Views/Shared/PaginacaoViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace GestãoDeTurmas.Views.Shared;
+
+public class PaginacaoViewModel
+{
+    public int PaginaAtual { get; set; }
+    public int TotalPaginas { get; set; }
+    public bool TemPaginaAnterior { get; set; }
+    public bool TemProximaPagina { get; set; }
+    public string Action { get; set; } = "Index";
+    public string Controller { get; set; } = "";
+    public Dictionary<string, string> Filtros { get; set; } = new();
+
+    public static Dictionary<string, string> MontarFiltros(params (string chave, object? valor)[] filtros)
+    {
+        return filtros
+            .Where(f => f.valor != null && f.valor.ToString() != "")
+            .ToDictionary(f => f.chave, f => f.valor!.ToString()!);
+    }
+
+    public Dictionary<string, string> FiltrosComPagina(int pagina)
+    {
+        var resultado = new Dictionary<string, string>(Filtros);
+        resultado["pagina"] = pagina.ToString();
+        return resultado;
+    }
+}
