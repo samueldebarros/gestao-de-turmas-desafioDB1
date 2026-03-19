83cddcbedd17a914fce53e7be747d34668cde6d3
Samuel de Barros <samuel.barros@db1.com.br>
2026-03-19 14:44:21 -0300

    fix: Turma-Ativo centralizado e displayName SerieEnum

--- Common/Enums/SerieEnum.cs
+++ Common/Enums/SerieEnum.cs
@@ -1,9 +1,16 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Common.Enums;
 
 public enum SerieEnum
 {
+    [Display(Name = "1º Ano")]
     PrimeiroAno = 1,
+
+    [Display(Name = "2º Ano")]
     SegundoAno = 2,
+
+    [Display(Name = "3º Ano")]
     TerceiroAno = 3
 }
